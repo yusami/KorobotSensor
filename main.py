500fa61b09018e647d3398cd45349b6899847faa
Yasushi Usami <usami2go@yahoo.co.jp>
2020-02-09 22:33:58 +0900

    Retry to fetch the sensor values if a HTTP error occurs.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import config
+import time
 import sensor
 from twitter import *
 
@@ -6,7 +7,14 @@ def main():
     tw = Twitter(auth=OAuth(config.TW_TOKEN, config.TW_TOKEN_SECRET, config.TW_CONSUMER_KEY, config.TW_CONSUMER_SECRET))
   
     # Post a message
-    items = sensor.main()
+    for i in range(10):
+        items = sensor.main()
+        if items != None:
+            break
+        # Wait a while and retry if an error occurs
+        print("retrying...")
+        time.sleep(5)
+
     # print("-time:{0[0]}, temperature:{0[1]}degrees, humidity:{0[2]}%, air pressure:{0[3]}hPa".format(item))
     msg = "Time:{0[0]}, Temperature:{0[1]} degrees, Humidity:{0[2]}%, Air pressure:{0[3]}hPa".format(items)
     print(msg)
--- sensor.py
+++ sensor.py
@@ -23,32 +23,37 @@ def main():
 
 # Call the request
     req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as res:
-        # Parse the response
-        body = json.load(res)
-        # print(body)
-        for item in body:
-            print(item)
-            # print(item[0])
-            # Convert the time from UTC to JST
+    try:
+        with urllib.request.urlopen(req) as res:
+            # Parse the response
+            body = json.load(res)
+            # print(body)
+            for item in body:
+                print(item)
+                # print(item[0])
+                # Convert the time from UTC to JST
+                dt = parser.parse(item[0]+"Z").astimezone(timezone('UTC'))
+                # dt = parser.parse(item[0], tzinfos={"UTC"}).astimezone(timezone('UTC'))
+                # print(dt, dt.tzname())
+                dt = dt.astimezone(timezone('Asia/Tokyo'))
+                print("-local time:", dt, dt.tzname())
+                # for i in item:
+                    # print(i)
+                # print("-temperature:{0}, humidity:{1}, air pressure:{2}".format(item[1], item[2], item[3]))
+
+            # Print the latest data
+            item = body[-1]
             dt = parser.parse(item[0]+"Z").astimezone(timezone('UTC'))
-            # dt = parser.parse(item[0], tzinfos={"UTC"}).astimezone(timezone('UTC'))
-            # print(dt, dt.tzname())
-            dt = dt.astimezone(timezone('Asia/Tokyo'))
-            print("-time:", dt, dt.tzname())
-            # for i in item:
-                # print(i)
-            # print("-temperature:{0}, humidity:{1}, air pressure:{2}".format(item[1], item[2], item[3]))
+            # item[0] = dt.astimezone(timezone('Asia/Tokyo')).strftime("%Y-%m-%d %H:%M:%S")
+            item[0] = dt.replace(microsecond=0).astimezone(timezone('Asia/Tokyo')).isoformat()
 
-        # Print the latest data
-        item = body[-1]
-        dt = parser.parse(item[0]+"Z").astimezone(timezone('UTC'))
-        # item[0] = dt.astimezone(timezone('Asia/Tokyo')).strftime("%Y-%m-%d %H:%M:%S")
-        item[0] = dt.replace(microsecond=0).astimezone(timezone('Asia/Tokyo')).isoformat()
+            print("-time:{0[0]}, temperature:{0[1]} degrees, humidity:{0[2]}%, air pressure:{0[3]}hPa".format(item))
 
-        print("-time:{0[0]}, temperature:{0[1]} degrees, humidity:{0[2]}%, air pressure:{0[3]}hPa".format(item))
-
-        return item
+            return item
+    except urllib.error.HTTPError as err:
+        print("HTTP error: ", err.code)
+    except urllib.error.URLError as err:
+        print("HTTP error: ", err.reason)
 
 if __name__ == '__main__':
     main()
